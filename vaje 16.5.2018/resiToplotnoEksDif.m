7916646ea8029fa71bf58e21246635a6175ad446
Miha Avsec <mihavsec@gmail.com>
2018-05-22 12:55:10 +0200

    dodani testni primeri

--- vaje 16.5.2018/resiToplotnoEksDif.m
+++ vaje 16.5.2018/resiToplotnoEksDif.m
@@ -1,11 +1,31 @@
 function [ U,lam ] = resiToplotnoEksDif(c,f,g,h,T,a,b,dt,dx)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+% Opis:
+%  resiToplotnoEksDif izracuna resitev toplotne enacbe
+%  du/dt = c^2 du^2/dx^2 z zacetnim pogojem u(0,x) = f(x) ter robnima
+%  pogojema u(t,a) = g(t) in u(t,b) = h(t) z uporabo eksplicitne diferencne
+%  sheme pri predpisani diskretizaciji
+%
+% Definicija:
+%  [U,lam] = resiToplotnoEksDif(c,f,g,h,T,a,b,dt,dx)
+%
+% Vhodni podatki:
+%  c        parameter, ki doloca enacbo,
+%  f        funkcija, ki doloca zacetni pogoj,
+%  g,h      funkciji, ki dolocata robni pogoj,
+%  T        maksimalni cas,
+%  a,b      parametra, ki omejujeta prostorsko spremenljivko,
+%  dt,dx    razmik diskretizacije v casovni in prostorski smeri
+%
+% Izhodna podatka:
+%  U        tabela, ki vsebuje izracunane priblizke za resitev problema pri
+%           casih med 0 in T z razmikom dt ter polozajih med a in b z
+%           razmikom dx; natancneje, element na mestu (n,j) predstavlja
+%           priblizek pri casu (n-1)*dt in polozaju a+(j-1)*dx,
+%  lam      Courantovo stevilo problema
 
-
-lam = c^2*dt/dx^2;
-J = (b-a)/dx+1;
-N = T/dt+1;
+lam = c^2*dt/dx^2
+J = (b-a)/dx+1
+N = ceil(T/dt+1)
 U = zeros(N,J);
 robna1 = zeros(N,1);
 robna2 = zeros(N,1);
--- vaje 16.5.2018/test.m
+++ vaje 16.5.2018/test.m
@@ -1,1 +1,52 @@
-[ U,lam ] = resiToplotnoEksDif(1/2,@(x)f(x),@(x) 0.*x,@(x) 0.*x,1,0,1,1/100,1/10)
+% c = 1/2;
+% f1 = @(x)f(x);
+% g = @(x) 0.*x;
+% h =  @(x) 0.*x;
+% T = 1;
+% a = 0;
+% b = 1;
+% dt = 1/100;
+% dx = 1/10;
+% 
+% x = [a:dx:b];
+% y = [0:dt:T];
+% [X,Y] = meshgrid(x,y);
+% [ U,lam ] = resiToplotnoEksDif(c,f1,g,h,T,a,b,dt,dx);
+% surf(X,Y,U)
+
+%test2
+
+% c = 1/2;
+% f1 = @(x)f(x);
+% g = @(x) 0.*x;
+% h =  @(x) 0.*x;
+% T = 1;
+% a = 0;
+% b = 1;
+% dt = 21/1000;
+% dx = 1/10;
+% 
+% x = [a:dx:b];
+% y = [0:dt:T];
+% [X,Y] = meshgrid(x,y);
+% [ U,lam ] = resiToplotnoEksDif(c,f1,g,h,T,a,b,dt,dx);
+% surf(X,Y,U(1:length(y),1:length(x)))
+
+
+%test3
+
+c = 1/2;
+f1 = @(x)f(x);
+g = @(x) x;
+h =  @(x) x;
+T = 1;
+a = 0;
+b = 1;
+dt = 1/100;
+dx = 1/10;
+
+x = [a:dx:b];
+y = [0:dt:T];
+[X,Y] = meshgrid(x,y);
+[ U,lam ] = resiToplotnoEksDif(c,f1,g,h,T,a,b,dt,dx);
+surf(X,Y,U(1:length(y),1:length(x)))
