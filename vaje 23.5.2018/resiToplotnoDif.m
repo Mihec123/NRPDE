2c1a5228a579af7144e612f35e1a77e7af3ec438
Mihec123 <mihavsec@gmail.com>
2018-05-23 09:32:42 +0200

    vaje 23.5.2018

--- vaje 23.5.2018/resiToplotnoDif.m
+++ vaje 23.5.2018/resiToplotnoDif.m
@@ -1,4 +1,4 @@
-function [ U,lam ] = resiToplotnoEksDif(c,f,g,h,T,a,b,dt,dx)
+function [ U,lam ] = resiToplotnoDif(c,f,g,h,T,a,b,dt,dx,th)
 % Opis:
 %  resiToplotnoEksDif izracuna resitev toplotne enacbe
 %  du/dt = c^2 du^2/dx^2 z zacetnim pogojem u(0,x) = f(x) ter robnima
@@ -15,6 +15,8 @@ function [ U,lam ] = resiToplotnoEksDif(c,f,g,h,T,a,b,dt,dx)
 %  T        maksimalni cas,
 %  a,b      parametra, ki omejujeta prostorsko spremenljivko,
 %  dt,dx    razmik diskretizacije v casovni in prostorski smeri
+%  th       parameter ustrezne diferencne sheme (0 = eksplicitna shema, 1=
+%           implicitna shema, 1/2 = Crank-Nicolsonova shema)
 %
 % Izhodna podatka:
 %  U        tabela, ki vsebuje izracunane priblizke za resitev problema pri
@@ -23,10 +25,13 @@ function [ U,lam ] = resiToplotnoEksDif(c,f,g,h,T,a,b,dt,dx)
 %           priblizek pri casu (n-1)*dt in polozaju a+(j-1)*dx,
 %  lam      Courantovo stevilo problema
 
-lam = c^2*dt/dx^2
-J = (b-a)/dx+1
-N = floor(T/dt+1)
+
+
+lam = c^2*dt/dx^2;
+J = (b-a)/dx+1;
+N = floor(T/dt+1);
 U = zeros(N,J);
+
 robna1 = zeros(N,1);
 robna2 = zeros(N,1);
 
@@ -42,11 +47,26 @@ U(1,:) = zacetni;
 U(:,1) = robna1;
 U(:,end) = robna2;
 
+B = diag(-2*ones(J-2,1)) + diag(ones(J-3,1),1) + diag(ones(J-3,1),-1);
+
 for i = 2:N
-    for j=2:J-1
-        U(i,j) = lam*U(i-1,j-1) + (1-2*lam)*U(i-1,j) + lam*U(i-1,j+1);
-    end
+    
+    Leva = eye(J-2) - lam*th*B;
+    Desna = (eye(J-2) +(1-th)*lam*B)*U(i-1,2:end-1)';
+    Desna(1) = Desna(1) + (1-th)*lam*U(i-1,1) + lam*th*U(i,1);
+    Desna(end) = Desna(end) + (1-th)*lam*U(i-1,end)+ lam*th*U(i,end);
+    
+    u = Leva\Desna;
+    U(i,2:end-1) = u';
+    
+    
+    
 end
+    
+    
+
+
+
 
 
 end
--- vaje 23.5.2018/test.m
+++ vaje 23.5.2018/test.m
@@ -1,3 +1,6 @@
+%testni primeri
+
+
 % c = 1/2;
 % f1 = @(x)f(x);
 % g = @(x) 0.*x;
@@ -7,11 +10,12 @@
 % b = 1;
 % dt = 1/100;
 % dx = 1/10;
+% th = 1/2;
 % 
 % x = [a:dx:b];
 % y = [0:dt:T];
 % [X,Y] = meshgrid(x,y);
-% [ U,lam ] = resiToplotnoEksDif(c,f1,g,h,T,a,b,dt,dx);
+% [ U,lam ] = resiToplotnoDif(c,f1,g,h,T,a,b,dt,dx,th);
 % surf(X,Y,U)
 
 %test2
@@ -25,12 +29,13 @@
 % b = 1;
 % dt = 21/1000;
 % dx = 1/10;
+% th = 1/2;
 % 
 % x = [a:dx:b];
 % y = [0:dt:T];
 % [X,Y] = meshgrid(x,y);
-% [ U,lam ] = resiToplotnoEksDif(c,f1,g,h,T,a,b,dt,dx);
-% surf(X,Y,U)
+% [ U,lam ] = resiToplotnoDif(c,f1,g,h,T,a,b,dt,dx,th);
+% surf(X,Y,U(1:length(y),1:length(x)))
 
 
 %test3
@@ -44,9 +49,12 @@ a = 0;
 b = 1;
 dt = 1/100;
 dx = 1/10;
+th = 1/2;
 
 x = [a:dx:b];
 y = [0:dt:T];
 [X,Y] = meshgrid(x,y);
-[ U,lam ] = resiToplotnoEksDif(c,f1,g,h,T,a,b,dt,dx);
-surf(X,Y,U(1:length(y),1:length(x)))
+[ U,lam ] = resiToplotnoDif(c,f1,g,h,T,a,b,dt,dx,th);
+surf(X,Y,U)
+
+i1 = find(X(1,:) == 0)
